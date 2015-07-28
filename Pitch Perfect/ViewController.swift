1440efb8f4cd0c04a0e190a7e531b517d5e65cdf
Felipe Knorr Kuhn <git@knorrium.info>
2015-07-27 21:06:13 -0700

    initial commit

---
+++ Pitch Perfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pitch Perfect
+//
+//  Created by Felipe Kuhn on 7/27/15.
+//  Copyright (c) 2015 Knorrium. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
