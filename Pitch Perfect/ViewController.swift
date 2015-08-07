18cd84ed42c0e66c528f4969da695439d2518bbd
Felipe Knorr Kuhn <git@knorrium.info>
2015-08-06 20:25:19 -0700

    hide the stop button on viewWillAppear

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        btnStop.hidden = true
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
