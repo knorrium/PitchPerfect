784dd1bed37052550b84b162a2e6f8f9809c59a3
Felipe Knorr Kuhn <git@knorrium.info>
2015-08-06 23:40:13 -0700

    add the RecordedAudio class

---
+++ RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Felipe Kuhn on 8/6/15.
+//  Copyright (c) 2015 Knorrium. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
